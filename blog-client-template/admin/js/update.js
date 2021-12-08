e3a5f5bd712bc2663a0caf769e1dd64a5b7ab399
Hourieh <hoorieh.23@gmail.com>
2021-12-08 15:04:08 +0100

    update work for title author and tags and content

--- blog-client-template/admin/js/update.js
+++ blog-client-template/admin/js/update.js
@@ -1,29 +1,30 @@
 window.onload = function() {
-    let queryString = location.search;       
+    let queryString = location.search;      
     console.log(queryString);
     let urlParams = new URLSearchParams(queryString);
     console.log(urlParams.get('id'));
 
     getPost(urlParams.get('id'));
 
-    updatePunEvent(urlParams.get('id'))
+    updatePostEvent(urlParams.get('id'))
 
-
+    
 }
-
-
 
 
 async function getPost(id) {
     try {
-        let response = await fetch('http://localhost:5000/posts' + id);
-        let data = await response.json();
+        let response = await fetch('http://localhost:5000/posts/' + id);
+        let post = await response.json();
 
         document.getElementById('content-textarea').value = post.content;
 
-        document.getElementById('title-textarea').value = post.content;
+        document.getElementById('title').value = post.title;
 
-        document.getElementById('author-textarea').value = post.content;
+        document.getElementById('author').value = post.author;
+
+        document.getElementById('tags').value = post.tags;
+
     } catch(error) {
         console.log(error);
     }
@@ -31,19 +32,22 @@ async function getPost(id) {
 
 
 
-function updatePunEvent(id) {
+function updatePostEvent(id) {
     let form = document.getElementById('update-post-form');
     form.addEventListener('submit', async function(e) {
         e.preventDefault();
         let formData = new FormData(form);
         formDataObject = {
-            "content": formData.get('content')
+            "content": formData.get('content'),
+            "title": formData.get('title'),
+            "author": formData.get('author'),
+            "tags": formData.get('tags'),
         }
 
-
+    
 
         try {
-            await fetch('http://localhost:5000/posts' + id, {
+            await fetch('http://localhost:5000/posts/' + id, {
                 method: 'PATCH', 
                 headers: {
                     'Content-Type': 'application/json',
