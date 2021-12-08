8d650ec041dd796b174b1d1562e7dc62d07d6400
John Landtblom <john.landtblom@live.se>
2021-12-08 09:41:15 +0100

    JS - Create Post

---
+++ blog-client-template/admin/js/create-post.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    submitBlogPost();
+}
+
+function submitBlogPost() {
+    let blogPost = document.getElementById('create-blog-post');
+
+    blogPost.addEventListener('submit', async function(e) {
+        e.preventDefault();
+
+        let blogContent = new FormData(blogPost);
+        let blogContentObject = {
+            "title"  : blogContent.get('title'),
+            "content": blogContent.get('content'),
+            "author" : blogContent.get('author'),
+            "tags"   : blogContent.get('tags')
+        }
+
+        try {
+            await fetch('http://localhost:5000/posts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(blogContentObject),
+            })
+            console.log(blogContentObject);
+            location.replace('index.html');
+        } catch(error) {
+            console.log(error);
+        }
+    });
+}
