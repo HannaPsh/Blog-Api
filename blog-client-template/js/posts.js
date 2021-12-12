567861730f7cbb819dc00dbfb8cc71c7fb19d52c
Hanna Pshanichnaya <hannapshanich@gmail.com>
2021-12-12 20:21:39 +0100

    pictures

--- blog-client-template/js/posts.js
+++ blog-client-template/js/posts.js
@@ -2,14 +2,23 @@ window.onload = function () {
   fetchAllPuns();
 };
 
+let arrayImg = ["4375581.jpg", "6584451.jpg", "6060285.png", "6584600.jpg"];
+
 async function fetchAllPuns() {
   try {
     let response = await fetch("http://localhost:5000/posts");
     let posts = await response.json();
     console.log(posts);
-
+    let element = -1;
     let postsHTML = "";
     for (let post of posts) {
+      if (element < arrayImg.length-1) {
+        element++;
+        console.log(element);
+      } 
+      else{
+        element = 0;
+      }
       let postDate = new Date(post.date);
       let formatedDate = `${postDate.getFullYear()}-${
         postDate.getMonth() + 1
@@ -21,17 +30,16 @@ async function fetchAllPuns() {
 
       console.log(post["_id"]);
       postsHTML += `
-                <li class="list-group-item">
-                    <h2>${post.title}</h2> <span class="date">${formatedDate}</span> <br> <span class="author">Author: ${post.author}</span>
-                    
+                
+                <li class="list-group-item" id="parent">
+                <div id="leftSide"> <img src = "${arrayImg[element]}" width="100%" height="70%" id="img"> </div>
+                   <div id="rightSide"> <h2>${post.title}</h2> <span class="date">${formatedDate}</span> <br> <span class="author">Author: ${post.author}</span>
+
                     <p>${post.content}</p>
-<p id="underline">_______________________________</p>
-                    <span class="date">Tags: ${post.tags}</span>
-
-                </li>
-            `;
+                    <hr>
+                    <span class="date">Tags: ${post.tags}</span></div></li>
+                    `;
     }
-
     document.getElementById("post-list").innerHTML = postsHTML;
   } catch (error) {
     console.log(error);
